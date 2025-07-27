f569f70b421074f6c5bd96cb0722e3ca1ec14a46
escyb3 <escoob30@gmail.com>
2025-07-27 12:50:08 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const fs = require("fs");
 const express = require("express");
 const session = require("express-session");
 const bcrypt = require("bcrypt");
-const path = require("path");
 const multer = require("multer");
 const pdfParse = require("pdf-parse");
 const { v4: uuidv4 } = require("uuid");
