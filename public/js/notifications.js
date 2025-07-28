096f968e01393144a4c4f0451961184aa44a4937
escyb3 <escoob30@gmail.com>
2025-07-28 08:43:48 +0300

    Create notifications.js

---
+++ public/js/notifications.js
@@ -0,0 +1,7 @@
+setInterval(async () => {
+  const res = await fetch("/messages");
+  const messages = await res.json();
+  const unseen = messages.filter(m => !m.seen).length;
+  const notif = document.getElementById("notifications");
+  notif.innerHTML = unseen ? `🔴 ${unseen} הודעות חדשות` : "";
+}, 10000);
