cd8b12331cb2995499abffdb490f3fbb564a6b01
escyb3 <escoob30@gmail.com>
2025-07-29 07:53:16 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -478,5 +478,5 @@ app.get('/api/migration-data', (req, res) => {
 // הפעלת השרת
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${3000}`);
+  console.log(`Server running on port ${PORT}`);
 });
