ffc52791c116b38f91e18297fab05412ba9d4219
escyb3 <escoob30@gmail.com>
2025-08-17 06:54:50 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1475,18 +1475,14 @@ const upload = multer({ storage: multer.memoryStorage() });
 
 // Firebase config
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
+apiKey: "AIzaSyAID_kPGA6Khczh0lIgd7E13LJS76JJ9nI",
+  authDomain: "mailbox-e0ce2.firebaseapp.com",
+  projectId: "mailbox-e0ce2",
+  storageBucket: "mailbox-e0ce2.firebasestorage.app",
+  messagingSenderId: "199399854104",
+  appId: "1:199399854104:web:6aec488e6aeee0dec3736d",
+  measurementId: "G-V3CSEQY5CR"
 };
-
-// Init Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const db = getFirestore(firebaseApp);
-const storage = getStorage(firebaseApp);
 
 // 📩 קבלת הודעה ושמירה בפיירבייס
 app.post("/api/messages", async (req, res) => {
