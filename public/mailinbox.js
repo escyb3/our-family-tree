e395638202cfba929885660b6c06c6e2f71a1ed5
escyb3 <escoob30@gmail.com>
2025-08-18 20:24:57 +0300

    Update mailinbox.js

--- public/mailinbox.js
+++ public/mailinbox.js
@@ -295,8 +295,7 @@ function startRealtimeSubscriptions() {
 
   // Inbox
 const inboxQ = query(
-  const appId = "1:199399854104:web:6aec488e6aeee0dec3736d";
-collection(db, `artifacts/${appId}/public/data/emails`);
+ collection(db, "artifacts/1:199399854104:web:6aec488e6aeee0dec3736d/public/data/emails")
     where("recipient", "==", state.emailAddress)
 );
 
