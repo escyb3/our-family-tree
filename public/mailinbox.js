5e09ff25da67d61e492b55cbd820c0f1c0b25e42
escyb3 <escoob30@gmail.com>
2025-08-20 13:11:10 +0300

    Update mailinbox.js

--- public/mailinbox.js
+++ public/mailinbox.js
@@ -5,7 +5,7 @@
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import {
-  getAuth, signInWithCustomToken, onAuthStateChanged, signInAnonymously
+  getAuth, signInWithCustomToken, onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore, collection, addDoc, onSnapshot, query, where, serverTimestamp,
          doc, setDoc, deleteDoc, getDoc, orderBy } 
