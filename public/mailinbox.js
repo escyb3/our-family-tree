df7011fbc4961930ea2e5d0d3cd63878ed924e0a
escyb3 <escoob30@gmail.com>
2025-08-22 18:17:51 +0300

    Update mailinbox.js

--- public/mailinbox.js
+++ public/mailinbox.js
@@ -7,7 +7,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
   getAuth, signInWithCustomToken, onAuthStateChanged,signInWithEmailAndPassword,  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, doc, collection, addDoc, onSnapshot, query, where, serverTimestamp,getDownloadURL,uploadBytes
+import { getFirestore, doc, collection, addDoc, onSnapshot, query, where, serverTimestamp,uploadBytes
          , setDoc, deleteDoc, getDoc, orderBy } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
