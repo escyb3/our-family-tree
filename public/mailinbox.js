045aaa96d29ed2e74590b8e378e5d9956a6138ef
escyb3 <escoob30@gmail.com>
2025-08-22 18:15:40 +0300

    Update mailinbox.js

--- public/mailinbox.js
+++ public/mailinbox.js
@@ -7,8 +7,8 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/fireba
 import {
   getAuth, signInWithCustomToken, onAuthStateChanged,signInWithEmailAndPassword,  createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-import { getFirestore, collection, addDoc, onSnapshot, query, where, serverTimestamp,getDownloadURL,uploadBytes
-         doc, setDoc, deleteDoc, getDoc, orderBy } 
+import { getFirestore, doc, collection, addDoc, onSnapshot, query, where, serverTimestamp,getDownloadURL,uploadBytes
+         , setDoc, deleteDoc, getDoc, orderBy } 
 from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 // -------------------- שפה / טקסטים --------------------
