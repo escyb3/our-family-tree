b6aebe06190c3eac9483590f0ef93a7d3203137a
escyb3 <escoob30@gmail.com>
2025-08-26 21:33:44 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -26,7 +26,7 @@ const translate = new Translate({ key: process.env.GOOGLE_API_KEY });
 // -------------------- Supabase --------------------
 const { createClient } = require('@supabase/supabase-js');
 const SUPABASE_URL = process.env.SUPABASE_URL || 'https://iyyxtqcdbsvpvhwrmxgv.supabase.co';
-const SUPABASE_KEY = process.env.SUPABASE_KEY || 'sb_secret_G--k9nK5CAvcdNN4_uIB2w_PmgApJaK';
+const SUPABASE_KEY = process.env.SUPABASE_KEY || 'sb_secret_G--k9nK5CAvcdNN4_uIB2w_PmgApJaK'
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // =========================
